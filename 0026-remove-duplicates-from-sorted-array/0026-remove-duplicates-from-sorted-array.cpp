2402ce3839698dce1a7c37cf658f0d2755640ca7
prajwal1-k <prajwal.kulkarni.p@gmail.com>
2025-08-03 23:53:07 +0530

    Update 0026-remove-duplicates-from-sorted-array.cpp

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int x = nums.size();
+        int x = nums.size();     // Here we are checking the length of the nums values
 
+        // if x is equal to '0' then we have to return 0 and there is no execution of the code
         if(x == 0)
         return 0;
 
