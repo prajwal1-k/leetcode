4cd8cf8eab02dad4a0769cacffd08ad1882307c1
prajwal1-k <prajwal.kulkarni.p@gmail.com>
2026-08-02 22:48:55 +0530

    Time: 0 ms (100%), Space: 10.3 MB (87.75%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -4,3 +4,22 @@ public:
         return true;
     }
 };
+
+// Spaced OPtimized
+// class Solution {
+// public:
+//     bool stoneGame(vector<int>& piles) {
+//         int n = piles.size();
+//         vector<int> dp = piles;
+
+//         for (int len = 2; len <= n; len++) {
+//             for (int i = 0; i <= n - len; i++) {
+//                 int j = i + len - 1;
+//                 dp[i] = max(piles[i] - dp[i + 1],
+//                             piles[j] - dp[i]);
+//             }
+//         }
+
+//         return dp[0] > 0;
+//     }
+// };
